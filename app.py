e1b8b55a543ade7cb103301a7e31caf97aa8e520
mandali-saketh-codes <mandalisakethpraveen@gmail.com>
2026-08-07 20:13:16 +0530

    Change Flask run configuration to bind to all interfaces

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ def predict():
     )
     # -----------------------------
 # Run Flask
-# -----------------------------
+# ==========================
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
